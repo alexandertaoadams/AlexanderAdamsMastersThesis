54cfdea969268ddf911bd6e1ae7e6ab2429d0c84
alexandertaoadams <159815946+alexandertaoadams@users.noreply.github.com>
2025-11-08 18:07:18 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,43 @@
+import numpy as np
+import pandas as pd
+import sklearn.metrics as skm
 
+
+
+
+def display_results(pred_labels, true_labels):
+
+    y_pred = np.array(pred_labels)
+    y_true = np.array(true_labels)
+    
+    total = int(len(y_true))
+    num_neg = int(np.sum(y_true == 0))
+    num_pos = int(np.sum(y_true == 1))
+    mcc = float(skm.matthews_corrcoef(y_true, y_pred))
+    f1 = float(skm.f1_score(y_true, y_pred))
+    precision = float(skm.precision_score(y_true, y_pred))
+    recall = float(skm.recall_score(y_true, y_pred))
+
+    data = {
+        "Metric": [
+            "Total samples",
+            "Negatives (0)",
+            "Positives (1)",
+            "MCC",
+            "F1 score",
+            "Precision",
+            "Recall"
+        ],
+        "Value": [
+            total,
+            num_neg,
+            num_pos,
+            round(mcc, 3),
+            round(f1, 3),
+            round(precision, 3),
+            round(recall, 3)
+        ]
+    }
+
+    results_table = pd.DataFrame(data).set_index("Metric")
+    return results_table

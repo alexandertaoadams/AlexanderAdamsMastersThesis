6557e4b3631c3e7fe66e1a7d6f116e40e49de9a9
alexandertaoadams <159815946+alexandertaoadams@users.noreply.github.com>
2026-01-03 17:19:10 +0000

    Update fit.py

--- src/fit.py
+++ src/fit.py
@@ -2,12 +2,12 @@ import gpjax as gpx
 from flax import nnx
 
 
-def custom_fit(model, objective, train_data, optimiser, num_iters, batch_size, verbose=False):
+def custom_fit(model, objective, train_dataset, optimiser, num_iters, batch_size, verbose=False):
 
     intermediate_model, history1 = gpx.fit(
         model=model,
         objective= lambda model, data: -objective(model, data),
-        train_data=train_data,
+        train_data=train_dataset,
         optim=optimiser,
         trainable=nnx.OfType((gpx.parameters.Real, gpx.parameters.LowerTriangular)),
         num_iters=num_iters,
@@ -18,7 +18,7 @@ def custom_fit(model, objective, train_data, optimiser, num_iters, batch_size, v
     optimised_model, history2 = gpx.fit(
         model=intermediate_model,
         objective= lambda model, data: -objective(model, data),
-        train_data=train_data,
+        train_data=train_dataset,
         optim=optimiser,
         trainable=gpx.parameters.Parameter,
         num_iters=num_iters,

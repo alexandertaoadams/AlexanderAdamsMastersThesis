3b326bfa87ba05632baabbdc3c6c88c0072eb1bf
alexandertaoadams <159815946+alexandertaoadams@users.noreply.github.com>
2026-01-11 16:53:11 +0000

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -22,47 +22,49 @@ def display_parameters(model):
     return df
 
 def display_results(pred_labels, true_labels):
-    """
-    Compute and return key binary classification metrics. 'Pred_labels and 'true labels' should have the same lngth.
 
-    Args:
-        pred_labels (array): Predicted class labels (0 or 1).
-        true_labels (array): True class labels (0 or 1).
-
-    Returns:
-        pd.DataFrame: A table containing test size, class counts, MCC, F1 score, precision, and recall.
-    """
     y_pred = np.array(pred_labels)
     y_true = np.array(true_labels)
-    
+
     total = int(len(y_true))
     num_neg = int(np.sum(y_true == 0))
     num_pos = int(np.sum(y_true == 1))
+
+    tp = int(np.sum((y_true == 1) & (y_pred == 1)))
+    tn = int(np.sum((y_true == 0) & (y_pred == 0)))
+    fp = int(np.sum((y_true == 0) & (y_pred == 1)))
+    fn = int(np.sum((y_true == 1) & (y_pred == 0)))
+
     mcc = float(skm.matthews_corrcoef(y_true, y_pred))
     f1 = float(skm.f1_score(y_true, y_pred))
-    precision = float(skm.precision_score(y_true, y_pred))
-    recall = float(skm.recall_score(y_true, y_pred))
 
     data = {
         "Metric": [
             "Test Size",
             "Negative Samples",
-            "Postive Samples",
+            "Positive Samples",
+            "True Positives (TP)",
+            "True Negatives (TN)",
+            "False Positives (FP)",
+            "False Negatives (FN)",
             "MCC",
             "F1 score",
-            "Precision",
-            "Recall"
         ],
         "Value": [
             total,
             num_neg,
             num_pos,
+            tp,
+            tn,
+            fp,
+            fn,
             round(mcc, 3),
             round(f1, 3),
-            round(precision, 3),
-            round(recall, 3)
         ]
     }
+
+    results_table = pd.DataFrame(data).set_index("Metric")
+    return results_table
 
     results_table = pd.DataFrame(data).set_index("Metric")
     return results_table

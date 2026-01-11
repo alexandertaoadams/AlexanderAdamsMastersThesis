8299f9f5831c8d5038944660d59f3d07ab9dc850
alexandertaoadams <159815946+alexandertaoadams@users.noreply.github.com>
2026-01-11 21:26:15 +0000

    Update fit.py

--- src/fit.py
+++ src/fit.py
@@ -304,7 +304,7 @@ def convergence_fit(
 
 
 
-def get_batch(train_data: Dataset, batch_size: int, key) -> Dataset:
+def get_batch(train_data, batch_size):
     """Batch the data into mini-batches. Sampling is done with replacement.
 
     Args:

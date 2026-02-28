bdb807fda184580738b5353480739f8681682a7d
alexandertaoadams <159815946+alexandertaoadams@users.noreply.github.com>
2026-02-28 19:14:20 +0000

    Create compute_engine.py

---
+++ src/compute_engine.py
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+class CustomComputeEngine(gpx.kernels.computations.AbstractKernelComputation):
+    """Custom compute engine class for the signature kernel.
+    """
+    def _gram(self, kernel, X, X_size):
+        lengthscales = kernel.lengthscales
+        amplitude = kernel.amplitude
+        weights = kernel.weights
+        return Gram_XX_jit(
+            X, X_size, kernel.n_timesteps, kernel.n_nontrivial_levels, lengthscales.get_value(), amplitude.get_value(), weights.get_value()
+            )
+
+    def _cross_covariance(self, kernel, X, Z, X_size, Z_size):
+        lengthscales = kernel.lengthscales
+        amplitude = kernel.amplitude
+        weights = kernel.weights
+        return Cross_XZ_jit(
+            X, Z, X_size, Z_size, kernel.n_timesteps, kernel.n_nontrivial_levels, lengthscales.get_value(), amplitude.get_value(), weights.get_value()
+            )
+
+    def gram(self, kernel, X, X_size):
+        return self._gram(kernel, X, X_size)
+
+    def cross_covariance(self, kernel, X, Z, X_size, Z_size):
+        return self._cross_covariance(kernel, X, Z, X_size, Z_size)
+
+    def diagonal(self, kernel, X, X_size):
+        return self._diagonal(kernel, X, X_size)
